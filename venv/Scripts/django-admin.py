3ebebe977ea663050b6e5a2a4fb21fed63b16e54
Wojtek Wanczyk <wojtekwanczyk@gmail.com>
2018-06-15 11:29:37 +0200

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Wojtek\Documents\Studia\IV semestr\Python\blog\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
