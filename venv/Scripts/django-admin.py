21fa08a2ce4fd9dd0fec0e489fbffb902963f2b0
Wojtek Wanczyk <wojtekwanczyk@gmail.com>
2018-06-15 12:14:12 +0200

    removed venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Wojtek\Documents\Studia\IV semestr\Python\blog\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
